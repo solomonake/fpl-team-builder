839f14bc27fa7bfdf1217e7edce4ca6073d93e3a
Precious Muwanguzi <preciousmuwanguzi@Solos-Macbook.local>
2025-08-04 18:12:09 -0400

    Update backend files

--- scripts/api.py
+++ scripts/api.py
@@ -400,4 +400,5 @@ def top_teams():
     return jsonify(top_teams)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5001))  # Use PORT env variable or default to 5001
+    app.run(host="0.0.0.0", port=port, debug=True)
